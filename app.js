6e60f5d024fe22bae02bdc6566ec60443ab00539
Andrew Watson <Andrew.justinw@gmail.com>
2021-12-29 10:26:38 -0500

    Most recent personal site

---
+++ app.js
@@ -0,0 +1,20 @@
+// let slideNumber = 1
+
+// let firstSlider = document.getElementById("top-right-slider")
+// let topSections = document.getElementById("top-info")
+
+
+
+// firstSlider.addEventListener("click", () => {
+
+//     slideNumber++
+//     console.log(slideNumber)
+//     topSections.scrollLeft += 650
+
+//     if (slideNumber == 3) {
+//         topSections.scrollLeft -= 650
+//         slideNumber = 1
+//     }
+
+// })
+
